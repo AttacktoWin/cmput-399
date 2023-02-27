3e3ef728eb32d5e0820b06dc9b80db5e08d0be1d
Shashank Bhat <shashank@ualberta.ca>
2023-02-27 09:47:55 -0700

    Premilinary strategy selection code

--- Server/websocket.py
+++ Server/websocket.py
@@ -21,6 +21,7 @@ class RPSWebSocket(WebSocket):
         for f in os.listdir('src/files'):
             if f.endswith('.pkl'):
                 self.QList.append(pickle.load(open('src/files/%s' % f, 'rb')))
+
     def handle(self):
         # id|board|player_points|enemy_points|chosen_unit|direction
         splits = self.data.decode().split('|')
@@ -29,51 +30,68 @@ class RPSWebSocket(WebSocket):
 
         # TODO: Load this using diffCheck
         Q = pickle.load(open('src/files/general1.pkl', 'rb'))
-        id = splits[0]
+        study_id = splits[0]
         units = splits[1].split(';')
         player_points = int(splits[2])
         enemy_points = int(splits[3])
         chosen_unit = int(splits[4])
         direction = splits[5]
 
-        parsed = []
-        for unit in units:
-            # name/hp/x/y/weapon/allegiance
-            unit = unit.split('/')
-            current_unit = Unit(unit[0], "", Stats(int(unit[1]), int(unit[2]), int(unit[3]), int(unit[4]), int(unit[5])))
-            parsed.append(current_unit)
+        if not os.path.exists('logs/%s.txt' % study_id):
+            with open('logs/%s.txt' % study_id, 'w') as log_file:
+                log_file.write("Player Unit, Player Direction, Player Strategy, Confidence, Chosen Strategy, Enemy Unit, Enemy Move")
 
-        state = State(parsed)
-        state.addPoint(0, player_points)
-        state.addPoint(1, enemy_points)
-        if id not in self.adapters.keys():
-            self.adapters[id] = adapter()
-            # TODO: figure out what to do
-            self.adapters[id].addMove(state.hash(0), )
-        if len(state.units) > chosen_unit:
-            state.units[chosen_unit].move(direction)
+        with open('logs/%s.txt' % study_id, 'a') as log_file:
 
-        Q, action_list, action = select_action(Q, state, state.enemies, state.players)
-        enemy_index = action_list[0]
-        enemy_direction = action_list[1]
-        state.enemies[enemy_index].move(enemy_direction)
-        state.resolve()
+            parsed = []
+            for unit in units:
+                # name/hp/x/y/weapon/allegiance
+                unit = unit.split('/')
+                current_unit = Unit(unit[0], "",
+                                    Stats(int(unit[1]), int(unit[2]), int(unit[3]), int(unit[4]), int(unit[5])))
+                parsed.append(current_unit)
 
-        # [name/hp/x/y/alive]|[name/hp/x/y/alive]
-        player_unit = state.units[chosen_unit]
-        enemy_unit = state.enemies[enemy_index]
-        self.send_message("%s/%i/%i/%i/%i|%s/%i/%i/%i/%i" % (
-            player_unit.name,
-            player_unit.stats.hp,
-            player_unit.stats.x,
-            player_unit.stats.y,
-            int(player_unit.alive),
-            enemy_unit.name,
-            enemy_unit.stats.hp,
-            enemy_unit.stats.x,
-            enemy_unit.stats.y,
-            int(enemy_unit.alive)
-        ))
+            state = State(parsed)
+            state.addPoint(0, player_points)
+            state.addPoint(1, enemy_points)
+            if study_id not in self.adapters.keys():
+                self.adapters[study_id] = adapter()
+                # TODO: figure out what to do
+                self.adapters[study_id].addMove(state.hash(0), [chosen_unit, direction])
+            if len(state.units) > chosen_unit:
+                state.units[chosen_unit].move(direction)
+
+            Q, action_list, action = select_action(Q, state, state.enemies, state.players)
+            enemy_index = action_list[0]
+            enemy_direction = action_list[1]
+            state.enemies[enemy_index].move(enemy_direction)
+            state.resolve()
+
+            log_file.write("%i,%s,%s,%f,%s,%i,%s" % (
+                chosen_unit,
+                direction,
+                "Strategy name",
+                1.00,
+                "Anti Strategy",
+                enemy_index,
+                enemy_direction
+            ))
+
+            # [name/hp/x/y/alive]|[name/hp/x/y/alive]
+            player_unit = state.units[chosen_unit]
+            enemy_unit = state.enemies[enemy_index]
+            self.send_message("%s/%i/%i/%i/%i|%s/%i/%i/%i/%i" % (
+                player_unit.name,
+                player_unit.stats.hp,
+                player_unit.stats.x,
+                player_unit.stats.y,
+                int(player_unit.alive),
+                enemy_unit.name,
+                enemy_unit.stats.hp,
+                enemy_unit.stats.x,
+                enemy_unit.stats.y,
+                int(enemy_unit.alive)
+            ))
 
     def connected(self):
         print(self.address, "connected")
